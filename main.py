c443d3e091be9899c47f83fac3d78add1b9e37d0
pikitosdoritos <fastovetsn@gmail.com>
2026-06-30 17:50:04 +0300

    Add user data handling and update .gitignore

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import asyncio
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
 from aiogram.filters import CommandStart 
+import json
 import os
 
 load_dotenv()
@@ -15,6 +16,15 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def greet(message: Message):
     await message.answer("Привет! Это бот для записи на индивидуальные тренировки")
+
+@dp.message(F.text)
+async def create_record(message: Message):
+    record = message.text
+
+    with open("user_data.json", "w", encoding="utf-8") as f:
+        json.dump(record, f, indent=4, ensure_ascii=False)
+
+    await message.answer("Ваша запись внесена в базу, ожидайте подтверждения от тренера.")
 
 async def main():
     await dp.start_polling(bot)
