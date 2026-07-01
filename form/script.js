74a1591eb9ce63f6a869065183c61d817d6c6f94
pikitosdoritos <fastovetsn@gmail.com>
2026-07-01 20:37:10 +0300

    Update registration form labels and add validation script for improved user input handling

---
+++ form/script.js
@@ -0,0 +1,33 @@
+const fullnameInput = document.getElementById("fullname")
+const phoneInput = document.getElementById("phone-number")
+const dateInput = document.getElementById("datetime")
+
+validateForm()
+
+function validateForm() {
+    const name = fullnameInput.value
+    const phone = phoneInput.value
+    const date = dateInput.value
+
+    const validityErrors = [checkName(name), checkPhone(phone), checkDate(date)]
+
+    showInvalid(validityErrors)
+}
+
+function checkName(name) {
+    if (!/\w{2,}(-\w{2,})? \w{2,}(-\w{2,})?/.test(name)) {
+        return "Укажите ФИО"
+    }
+}
+
+function checkPhone(phone) {
+    if (!/\+\d{1,3} ?\(\d{2}\) ?\d{3}-?\d{2}-?\d{2}/.test(phone)) {
+        return "Введите корректный номер телефона"
+    }
+}
+
+function checkDate(date) {
+    if (!date) {
+        return "Выберите дату тренировки"
+    }
+}
